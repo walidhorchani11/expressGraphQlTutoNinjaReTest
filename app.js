3fe23d1ffad33a6b75e362b0be27a43b5543723e
walid horchani <walid.horchani@oyez.fr>
2020-01-27 20:16:45 +0100

    middleware in app axpress

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res)=> {
-  res.json('votre requete a bien ete recus ...');
-})
+//une application express est fonadamentalement une serie de fonctions appelé middleware, chaque element de middleware recoit les objets request et response , peut les lire, les analyser et les manipuler, il peut recevoir aussi la methode next pour passer au middleware suivant 
+
+app.use((req, res, next) => {
+  console.log('reqeute recus avec success');
+  //passer l executio au middleware suivant
+  next();
+});
+
+//2eme middlware
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+//3eme middlware
+app.use((req, res, next) => {
+  res.json({message : 'votre requete a bien ete recus ...'});
+  next();
+});
+
+app.use((req, res, next)=> {
+  console.log('response envoyé avec success...'); 
+});
 
 module.exports = app;
