886221b8dc648d732030b3a1fd7b15f54ab353b5
walid horchani <walid.horchani@oyez.fr>
2020-01-30 16:21:53 +0100

    video 25 setup apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import BookList from './components/BookList';
 
+//apollo client setup 
+import ApolloClient from 'apollo-boost';
+
+//pour que react understand appollo
+import {ApolloProvider} from 'react-apollo';
+
+
+const client = new ApolloClient({
+  //to make request to this endpoint from this app
+  uri: 'http://localhost:4000/graphql',
+})
+
 function App() {
   return (
-    <div id="main">
-      <h1>walid horchani tuto graphQL and Appollo</h1>
-      <BookList />
-    </div>
+    //il faut wrapper aour app par appollo provider et on lui passe our client deja cree
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>walid horchani tuto graphQL and Appollo</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
